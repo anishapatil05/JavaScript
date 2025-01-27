5f12f7a484a652dacbd5499822dd6d1123c0068d
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-27 21:50:23 +0530

    Add files via upload

---
+++ Basics Of JavaSript/DataTypes.js
@@ -0,0 +1,49 @@
+/* Data Types in JavaScript : 1 Primitive Data Types = i.number
+                                                       ii.String
+                                                       iii.boolean
+                                                       iv.undefined 
+                                                       v.null
+                                                       vi.BigInt
+                                                       vii.symbol
+                               2. Non-primitive Data Types : i.object */
+// String Data Type    
+var name = "Anisha";
+console.log(name);
+console.log(typeof name);
+
+//  number Data Type
+var id = 7;
+console.log("id: " + id + " data type: " + typeof id);
+
+// boolean Data type
+var isPlaced = true;
+console.log(isPlaced);
+
+// undefined Data type
+var name;
+console.log(name);      // undefined;
+
+// null Data Type
+var name = null;
+console.log(name)        // null;
+
+var name = "Anu";
+console.log(name);
+
+// BigInt : for large data scales = 2 power 53-1 
+
+// Symbol : creating data using symbol() constructor
+
+// object Data type : object 
+var info = {
+    city: "jaysingpur",
+    id: 8
+}
+console.log(typeof info)
+
+// Array
+var information = [2, 'hello', true]
+console.log(typeof information)
+
+
+
