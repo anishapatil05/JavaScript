8541837544a8cd59fb6f56a83238dcbb2b541b6f
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-08 18:17:16 +0530

    Add files via upload

---
+++ Operators/SpecialOprator.js
@@ -0,0 +1,16 @@
+// Special Oprators : 1.delete
+
+var productDetails = {
+    name: "Laptop",
+    price: 70000,
+    warnty: "5 months",
+    coolor: "sliver",
+}
+
+delete productDetails.warnty;
+console.log(productDetails);
+
+// typeOf
+var arr = ["Anu", "Shauu", "Aapu", "Siddu", "Sanu"];
+
+console.log(typeof arr)
