7050e41c92a5a178e5cc2110ca27f2e32ae8d14f
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-11 17:38:46 +0530

    Add files via upload

---
+++ Operators/Expoentiation.js
@@ -0,0 +1,17 @@
+// Expoentitaion
+let a = 5;
+let b = 4;
+
+let result = a ** b;
+console.log(result);   //5*5*5*5 : 625
+
+
+// Example : find multiple of 5
+let num = 45;
+if(num % 5 == 0)
+{
+    console.log("5 is multiple of",num);
+}else{
+    console.log(num,"Not multiple of 5");
+}
+
