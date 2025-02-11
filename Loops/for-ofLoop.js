83ff2fed7d37c96daadacff89f8d101b5d02f5cc
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-11 18:22:16 +0530

    Add files via upload

---
+++ Loops/for-ofLoop.js
@@ -0,0 +1,15 @@
+// for-of loop in javascript
+
+// creating array
+const arr = ["Anisha","Shreyash","Sidhant","Shreyash"];
+
+for(let elements of arr){
+    console.log(elements);
+}
+
+// array 2 
+const values =[1,2,3,4,5,6,7];
+
+for(let elements of values){
+    console.log(elements);
+}
