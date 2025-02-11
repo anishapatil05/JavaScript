88655b86e38259812dd7b505ef1c8968d5ad8035
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-11 15:34:08 +0530

    Add files via upload

---
+++ Variables/variablesDemo.js
@@ -0,0 +1,21 @@
+
+// var : variable var can be re-decalred & update  
+var name = "anisha";
+var name = "anisha";
+
+name = "shreya";
+
+console.log(name);
+
+// 2.let : variable let can not be re-declared but we update that..block scope variable
+
+let age = 34;
+age = 67;
+
+console.log(age);
+
+//  3.const : variable const can not be re-declared and update also...
+const PI = 3.14;
+console.log(PI);
+
+
