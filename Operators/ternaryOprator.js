9bc58794a09784bce8b87d395eb31bff3dbddd38
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-11 17:38:23 +0530

    Add files via upload

---
+++ Operators/ternaryOprator.js
@@ -0,0 +1,10 @@
+// ternary Oprator
+let age = 45;
+ age > 18 ? console.log("adult") : console.log("Not adult");
+
+//  ternary optaor using function
+ function isEligibleForVoting(age){
+     let isElibal = age > 18 ? "Eligibal" : "Not eligebal";
+     console.log(isElibal);
+ }
+isEligibleForVoting(2);
