2a584979e9e9aa8e79a7d6fc038300ccfadb4238
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-11 18:21:42 +0530

    Add files via upload

---
+++ Loops/forLoop.js
@@ -0,0 +1,33 @@
+// for loop in javascript
+
+// task 1: print 5 times javascript
+for (let i = 1; i<= 5; i++)
+{
+    console.log("javascript");
+}
+
+// task 2 : print numbers in reverse order
+for(let i=8; i >= 1; i--)
+{
+    console.log(i);   // 8 7 6 5 4 3 2 1
+}
+
+// print sum of 1 to 5 numbers
+let sum = 0;
+for(let i =1; i<= 5; i++)
+{
+    sum += i; 
+}
+console.log("sum: "+sum);
+
+// Print all even numbers from 1 to 100
+
+for(let i=1; i<= 100; i++)
+{
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+
+let number =56;
+
