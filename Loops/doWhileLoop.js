70a1e5e88934f7b1573e27df09cde2bc7d61751b
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-11 13:24:19 +0530

    Add files via upload

---
+++ Loops/doWhileLoop.js
@@ -0,0 +1,11 @@
+// do-while loop in javascript
+
+function printingNum(){
+    var value = 10;
+    do{
+        console.log(value);
+        value++;
+        
+    }while(value < 60 );
+}
+printingNum();
