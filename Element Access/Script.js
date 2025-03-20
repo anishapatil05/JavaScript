7328804f99c1058892dd246056888792aea58844
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-03-20 10:21:39 +0530

    Add files via upload

---
+++ Element Access/Script.js
@@ -0,0 +1,23 @@
+let classElement = document.getElementsByClassName(".heading");
+console.log(classElement); 
+
+let para = document.getElementsByTagName("p");
+console.log(para);
+
+let buttElement = document.getElementById("#button");
+console.dir(buttElement);
+
+let buttonElement = document.querySelector("#button");
+console.log(buttonElement);
+
+let paraElement = document.querySelector(".heading");
+console.log(paraElement)
+
+let divFirstElement = document.body.firstChild;
+console.log(divFirstElement);            //it returns textnode 
+
+let bodyLastElement = document.body.lastChild;
+console.log(bodyLastElement);    
+
+let bodyChildernElement = document.body.lastChild;
+console.log(bodyChildernElement);    
