f319fe07fe02db332d75d3c3e89f0c73ee768557
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-03-20 11:01:36 +0530

    Add files via upload

---
+++ DOM Manipulation/Insert & Delete Oper/Script.js
@@ -0,0 +1,29 @@
+let newbtn = document.createElement("button");
+newbtn.innerText = "click me!";
+console.log(newbtn);
+
+let div = document.querySelector("div");
+// At First
+div.prepend(newbtn);
+
+// AtLast
+div.append(newbtn)
+
+// before div
+div.before(newbtn)
+
+// After div
+div.after(newbtn)
+
+// Adding one Heading
+let h1 = document.createElement("h1");
+ h1.innerHTML ="<i>JAVASCRIPT</i>"
+console.log(h1);
+
+let body = document.querySelector("body");
+ body.prepend(h1)
+
+//  To remove node 
+newbtn.remove();
+let para = document.querySelector("p");
+para.remove();
