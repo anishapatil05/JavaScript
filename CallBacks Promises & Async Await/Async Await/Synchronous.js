2d6f8d41b970da416052899e91f7286f74140e94
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-03-23 19:47:49 +0530

    Add files via upload

---
+++ CallBacks Promises & Async Await/Async Await/Synchronous.js
@@ -0,0 +1,11 @@
+
+function calaculator(a, b)
+{
+    // Synchronous excution flow of instructions
+    console.log("Normal Insertion order")
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a*b);
+    console.log(a/b);
+
+}
