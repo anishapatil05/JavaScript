c1cb26d178dae522fb32ad96e40fc143315e7944
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-03-23 12:24:15 +0530

    Add files via upload

---
+++ Class & Objects/ErrorHandling.js
@@ -0,0 +1,18 @@
+//  Error handling Example using try-catch block
+let a = 10;
+let b = 20;
+
+console.log(a + b);
+console.log(a - b);
+try{
+    console.log(a * c);
+}catch(err)
+{
+    console.log(err);
+}
+//  It ia prints after adding try-catch block
+console.log(a % b);
+console.log(a / b);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
